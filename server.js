fcfb3bbb9106d5340b5b7665f730405dfbd7ade1
Ankit <ankitmt0011@gmail.com>
2025-04-18 21:24:16 +0530

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.mongodb+srv://rvmohit01:<db_password>@cluster1.oxv9p2d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
